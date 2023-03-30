37bdbc61e602ae0779972a2f6f5556cbb677242a
Roni Wellington <roniwellington@gmail.com>
2023-03-30 12:54:18 -0300

    estudo java

--- imersãoJava/alura-stickers/src/App.java
+++ imersãoJava/alura-stickers/src/App.java
@@ -12,29 +12,34 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP E BUSCAR OS TOP 250 FILMES
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request =  HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
-
 
         // Extrair só os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-
         // Exibir e manipular os dados
         var geradora = new GeradoraDeFigurinhas();
-        for (Map<String,String> filme : listaDeFilmes) {
 
-            String urlImagem = filme.get("image");
+        for (int i = 0; i < 3; i++) {
+
+            Map<String,String> filme = listaDeFilmes.get(i);
+
+            String urlImagem = 
+                //filme.get("image");
+                filme.get("url")
+                .replaceAll("(@+)(.*).jpg$", "$1.jpg");
+
             String titulo = filme.get("title");
-            InputStream inputStream = new URL(urlImagem).openStream();
 
-            String nomeArquivo = titulo + ".png";
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = "saida/" + titulo + ".png";
 
             geradora.cria(inputStream, nomeArquivo);
 
