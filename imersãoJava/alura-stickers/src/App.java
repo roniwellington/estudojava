ada82d9b3a1aea403b677dc0bc07ec8febd90baf
Roni Wellington <roniwellington@gmail.com>
2023-03-31 00:31:51 -0300

    estudo de java | imersão alura

--- imersãoJava/alura-stickers/src/App.java
+++ imersãoJava/alura-stickers/src/App.java
@@ -7,13 +7,16 @@ public class App {
 
         // fazer uma conexão HTTP E BUSCAR OS TOP 250 FILMES
         //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        //String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+
+        String url = "http://localhost:8080/linguagens";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
         
         var http = new ClienteHttp();
         String json = http.buscaDados(url);
 
         // Exibir e manipular os dados
-        ExtratorDeConteudoDaNasa extrator = new ExtratorDeConteudoDaNasa();
+        //ExtratorDeConteudoDaNasa extrator = new ExtratorDeConteudoDaNasa();
         List<Conteudo> conteudos = extrator.extraiConteudos(json);
 
         var geradora = new GeradoraDeFigurinhas();
--- linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -1,23 +1,31 @@
 package br.com.alura.linguagens.api;
 
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class LinguagemController {
-
-    private List<Linguagem> linguagens =
-        List.of(
-            new Linguagem("java", "https://github.com/abrahamcalf/programming-languages-logos/blob/master/src/java/java_256x256.png", 1),
-            new Linguagem("PHP", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png", 2)
-
-        );
-
-    @GetMapping("/linguagens") 
-    public List<Linguagem> obterLinguagens() {
-        return  linguagens;
-    }
     
+    @Autowired
+    private LinguagemRepository repositorio;
+
+    @GetMapping("/linguagens")
+    public List<Linguagem> obterLinguagens() {
+        List<Linguagem> linguagens = repositorio.findAll();
+        return linguagens;
+    }
+
+    @PostMapping("/linguagens")
+    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
+        Linguagem linguagemSalva = repositorio.save(linguagem);
+        return linguagemSalva;
+    } 
+
 }
