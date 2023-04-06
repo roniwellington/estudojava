c903009ee6059856835180c570d3a984ed4dd943
Roni Wellington <roniwellington@gmail.com>
2023-04-05 21:04:43 -0300

    eSTUDO JAVA

---
+++ java-alura/listaEDados/Teste.java
@@ -0,0 +1,11 @@
+
+public class Teste {
+    
+    public static <Produto> void main(String[] args) {
+        Produto p1 = new Produto("Caneta", 1.50);
+        Produto p2 = p1;
+
+        p2.setPreco(2.00);
+        System.out.println(p1.getPreco);
+    }
+}
