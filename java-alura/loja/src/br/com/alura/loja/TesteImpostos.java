6efd5b4a99bb588a3ff7ae48c3dbb41b6bd0d26a
roniwellington <roniwellington@gmail.com>
2023-04-27 23:32:24 -0300

    estudo de java

---
+++ java-alura/loja/src/br/com/alura/loja/TesteImpostos.java
@@ -0,0 +1,15 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+
+public class TesteImpostos {
+
+	public static void main(String[] args) {
+
+		//Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+		//System.out.println(calculadora.calcular(orcamento, new ICMS()));
+
+	}
+
+}
